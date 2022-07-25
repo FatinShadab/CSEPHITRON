d04571af9129e2bbcc775ec5c887ce4d103be704
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2022-07-25 23:10:56 +0600

    Update main.cpp

--- C++ Lang/Exam_one/solve/main.cpp
+++ C++ Lang/Exam_one/solve/main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
+#include <iomanip>
 #include <bits/stdc++.h>
 #include <string>
 #include <list>
 
 using namespace std;
 
+struct user_order{
+    int table_no;
+    int number_of_items;
+    int item_codes[12];
+    int item_quantities[12];
+};
+
 //////// Task: 1 (start)
 class Restaurant{
     protected:
-        int total_tax;
+        double total_tax;
 
     public:
         int food_item_codes[12];
@@ -20,8 +28,35 @@ class Restaurant{
             this->total_tax = 0;
         }
 
+        void add_tax(double tax){
+            this->total_tax += tax;
+            cout<<"The total tax of the restaurant is "<<this->total_tax<<endl;
+        }
+
 };
 //////// Task: 1 (end)
+
+int valid_food_item_code(int code, Restaurant* restaurant){
+    /*
+    This function will return 1 if the code is valid or 0.
+    */
+    for(int i=0; i<12; i++){
+        if (code == restaurant->food_item_codes[i]){
+            return 1;
+        }
+    }
+
+    return 0;
+};
+
+int get_code_idx(int code, Restaurant* restaurant){
+    for(int i=0; i<12; i++){
+        if (code == restaurant->food_item_codes[i]){
+            return i;
+        }
+    }
+    return -1;
+};
 
 /////// Task : 2 (start)
 Restaurant* create_restaurant(){
@@ -61,11 +96,89 @@ void show_menu_card(Restaurant* restaurant){
         cout<<"|Item Price - "<<restaurant->food_item_prices[i]<<endl;
         cout<<"|-------------------------------------------------------------------------|"<<endl;
     }
-}
+};
 ////// Task : 3 (end)
+
+///// Task : 4 (start)
+struct user_order take_user_order(Restaurant* restaurant){
+    struct user_order order;
+
+    cout<<"\nEnter Table No : ";
+    cin>>order.table_no;
+    cout<<"\nEnter Number of Items : ";
+    cin>>order.number_of_items;
+
+    if (order.number_of_items > 12){
+
+        while (order.number_of_items > 12){
+            cout<<"\nWe only have 12 Items in Menu !"<<endl;
+            cout<<"\nEnter Number of Items : ";
+            cin>>order.number_of_items;
+        }
+
+    }
+
+    for (int i=0; i<order.number_of_items; i++){
+
+        cout<<"\nEnter Item "<<i+1<<" Code : ";
+        cin>>order.item_codes[i];
+// Task : 8 (start)
+        if (valid_food_item_code(order.item_codes[i], restaurant) != 1){
+            while (valid_food_item_code(order.item_codes[i], restaurant) != 1){
+                cout<<"\n"<<"Invalid code : "<<order.item_codes[i]<<". "<<"Enter Item "<<i+1<<" Code again : ";
+                cin>>order.item_codes[i];
+            }
+        }
+// Task : 8 (end)
+        cout<<"\nEnter Item "<<i+1<<" Quantity : ";
+        cin>>order.item_quantities[i];
+    }
+    
+    return order;
+}
+///// Task : 4 (end)
+
+//// Task : 5 (start)
+void handle_order(Restaurant* restaurant, struct user_order order){
+    int total_bill = 0;
+    cout<<"==========================================================================="<<endl;
+    cout<<"                                 BILL SUMMARY                              "<<endl;
+    cout<<"==========================================================================="<<endl;
+    cout<<"Table No : "<<order.table_no<<"\n"<<endl;
+    for (int i=0; i<order.number_of_items; i++){
+        int idx = get_code_idx(order.item_codes[i], restaurant);
+        cout<<"Item Code :        "<<order.item_codes[i]<<endl;
+        cout<<"Item Name :        "<<restaurant->food_item_names[idx]<<endl;
+        cout<<"Item Price :       "<<restaurant->food_item_prices[idx]<<endl;
+        cout<<"Item Quantity :    "<<order.item_quantities[i]<<endl;
+        cout<<"Item Total Price : "<<restaurant->food_item_prices[idx]*order.item_quantities[i]<<endl;
+        cout<<"---------------------------------------------------------------------------"<<endl;
+        total_bill += restaurant->food_item_prices[idx]*order.item_quantities[i];
+    }
+/// Task : 6 (start)
+    double tax = (total_bill * 5)/100.0;
+    double net_total = total_bill + tax;
+    cout<<"TAX              : "<<tax<<endl;
+    cout<<"---------------------------------------------------------------------------"<<endl;
+    cout<<"NET TOTAL        : "<<net_total<<endl;
+    cout<<"===========================================================================\n"<<endl;
+// Task : 7 (start)
+    restaurant->add_tax(tax);
+// Task : 7 (end)
+/// Task : 6 (end)
+};
+//// Task : 5 (end)
 
 int main(){
     Restaurant* restaurant = create_restaurant();
-    show_menu_card(restaurant);
+
+    while (1){
+        show_menu_card(restaurant);
+
+        struct user_order order = take_user_order(restaurant);
+
+        handle_order(restaurant, order);
+    }   
+
     return 0;
 }
