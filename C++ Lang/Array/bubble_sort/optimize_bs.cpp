45ff9d673fe1e524dbee4ddd4f93fc0bce3a69c9
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2022-08-01 09:21:53 +0600

    Create optimize_bs.cpp

---
+++ C++ Lang/Array/bubble_sort/optimize_bs.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void bubble_short_ase(int arr[], int size){
+    for(int i=1; i<size-1; i++){
+        cout<< "iteration no : "<< i<< endl;
+        for(int j=0; j<size-i; j++){
+            if (arr[j]>arr[j+1]){
+                int temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+            //print the array
+            for(int i=0; i<size; i++){
+                cout<<arr[i]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+
+    cout<<"\nAfter bubble short (asending) - ";
+    for(int i=0; i<size; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    int size;
+
+    cout<<"Enter the array size - ";
+    cin>>size;
+
+    int arr[size];
+
+    cout<<"\nEnter the array elements - ";
+    for(int i=0; i<size; i++){
+        cin>>arr[i];
+    }
+
+    bubble_short_ase(arr, size);
+    cout<<endl;
+
+    return 0;
+}
