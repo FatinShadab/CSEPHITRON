82afba733b4626cbd46b635ad6533dbab0edd69a
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2022-09-02 14:17:31 +0600

    Update main.cpp
    use of new stack header file

--- C++ Lang/stack/main.cpp
+++ C++ Lang/stack/main.cpp
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
-#include "stack_v1.h"
+//#include "stack_v1.h"
+#include "stack_v2.h"
 
 using namespace std;
 
-int main(){
+/*
+void stack_v1_test(){
     Stack st;
 
     st.push(1);
@@ -12,6 +14,26 @@ int main(){
 
     while (st.empty() != true){
         cout<<st.pop()<<endl;
+    }
+}
+*/
+
+int main(){
+
+    Stack <pair<int, char>> st;
+
+    cout<<"stack size -> "<<st.get_size()<<endl;
+    st.push(make_pair(1, 'a'));
+    cout<<"stack size -> "<<st.get_size()<<endl;
+    st.push(make_pair(2, 'b'));
+    cout<<"stack size -> "<<st.get_size()<<endl;
+    st.push(make_pair(3, 'c'));
+    cout<<"stack size -> "<<st.get_size()<<endl;
+
+    while (!st.empty()){
+        pair <int, char> chk;
+        chk = st.pop();
+        cout<<chk.first<<" | "<<chk.second<<endl;
     }
 
     return 0;
