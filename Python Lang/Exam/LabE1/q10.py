05aecf2b3d72fd59e0437602fce5ef209facc5df
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2022-10-25 08:30:01 +0600

    Create q10.py

---
+++ Python Lang/Exam/LabE1/q10.py
@@ -0,0 +1,14 @@
+"""
+10. Given a string ‘s’ you need to find the words that are in list ‘a’ and use the next words on
+string ‘s’ to create a new string. Save it inside a file called ‘out.txt’. Remember to close the
+file after writing.
+Write a function named create_new_string() and write your code inside this function.
+Example:
+
+a = ['oh', 'Emelia', 'to']
+
+s = "Oh, I got two tickets for Dhaka. I and Emelia love to visit
+different places very much. This time we are going to Bangladesh."
+
+output = "I love Bangladesh" (inside a file)
+"""
