2c039aef0ffb11baaff89514ec1fc47941f591b1
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2022-10-25 08:29:45 +0600

    Create q4.py

---
+++ Python Lang/Exam/LabE1/q4.py
@@ -0,0 +1,14 @@
+"""
+4. Fix this code, get help from google.
+Copy the error message and search on web.
+
+>>>def print_hi():
+>>>    print("hi")
+>>>print(print_hi()) # you can't change this line
+"""
+
+def print_hi() -> str:
+    return "hi"
+
+if __name__ == "__main__":
+    print(print_hi()) # you can't change this line
