901907bf14f5fd71d89b9401750d4adb659cef7e
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2022-10-25 09:06:59 +0600

    Update q10.py

--- Python Lang/Exam/LabE1/q10.py
+++ Python Lang/Exam/LabE1/q10.py
@@ -10,5 +10,32 @@ a = ['oh', 'Emelia', 'to']
 s = "Oh, I got two tickets for Dhaka. I and Emelia love to visit
 different places very much. This time we are going to Bangladesh."
 
-output = "I love Bangladesh" (inside a file)
+output = "I love visit Bangladesh" (inside a file)
 """
+
+def create_new_string() -> None:
+    a = ['oh', 'Emelia', 'to']
+    a = [word.lower() for word in a]
+
+    s = "Oh, I , got two tickets for Dhaka. I and Emelia love to visit different places very much. This time we are going to Bangladesh."
+    words = s.split(" ")
+
+    new_string_words = []
+    for idx, word in enumerate(words):
+        for char in word:
+            if ord(char) not in range(65, 123):
+                words[idx] = word.replace(char, "")
+
+    for idx, word in enumerate(words):
+        if word.lower() in a:
+            new_string_words.append(words[idx+1])
+
+
+    print(" ".join(new_string_words))
+
+    with open("out.txt", 'w') as f:
+        f.write(" ".join(new_string_words))
+
+
+if __name__ == "__main__":
+    create_new_string()
