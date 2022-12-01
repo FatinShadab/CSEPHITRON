f18d37c5f68f55be63dc959f165443288678008f
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2022-12-01 16:54:36 +0600

    Update week_rainfall.py
    modified to get all the day names with the max or min rainfall

--- Python Lang/Exam/LabE4/week_rainfall.py
+++ Python Lang/Exam/LabE4/week_rainfall.py
@@ -11,13 +11,17 @@ def get_week_day_name(day : int) -> str:
     return calendar.weekheader(9).split()[day]
  
 def get_rainfall_data() -> tuple:
-    return tuple(input(f"Enter the rainfall data of {get_week_day_name(day)} : ") for day in range(7))
+    return tuple(int(input(f"Enter the rainfall data of {get_week_day_name(day)} : ")) for day in range(7))
  
 def get_minimum_rainfall_day(data : tuple) -> str:
-    return get_week_day_name(data.index(min(data)))
+    _min = min(data)
+    all_min_indices = [idx for idx, d in enumerate(data) if d == _min]
+    return ' '.join([get_week_day_name(index) for index in all_min_indices]).rstrip()
  
 def get_maximum_rainfall_day(data : tuple) -> str:
-    return get_week_day_name(data.index(max(data)))
+    _max = max(data)
+    all_max_indices = [idx for idx, d in enumerate(data) if d == _max]
+    return ' '.join([get_week_day_name(index) for index in all_max_indices]).rstrip()
  
 def main() -> None:
     rainfall_data = get_rainfall_data()
@@ -25,7 +29,7 @@ def main() -> None:
     day_with_min_rainfall = get_minimum_rainfall_day(rainfall_data)
     day_with_max_rainfall = get_maximum_rainfall_day(rainfall_data)
  
-    print(f"\nMaximum rainfall recorded on '{day_with_max_rainfall}'\nMinimum rainfall recorded on '{day_with_min_rainfall}")
+    print(f"\nMaximum rainfall recorded on '{day_with_max_rainfall}'\nMinimum rainfall recorded on '{day_with_min_rainfall}'")
  
  
 if __name__ == "__main__":
