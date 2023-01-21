7e546bf218e4789cee943c057ed083b854baf3a8
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2023-01-21 12:26:17 +0600

    Create FloodFillBFS.cpp

---
+++ ALgo/BFS/FloodFillBFS.cpp
@@ -0,0 +1,106 @@
+/*
+Problem link - https://cses.fi/problemset/task/1192
+
+INPUT:
+5 8
+########
+#..#...#
+####.#.#
+#..#...#
+########
+
+Output -> 3
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 1000;
+
+int n, m;
+int visited[N][N];
+int maze[N][N];
+
+int dX[] = {0, 0, -1, 1};
+int dY[] = {1, -1, 0, 0};
+
+int inMaze(pair <int, int> node){
+    int x = node.first;
+    int y = node.second;
+
+    return ((x >= 0 && x < n) && (y >= 0 && y < m)) ? 1:0;
+}
+
+int available(pair <int, int> node){
+    int x = node.first;
+    int y = node.second;
+
+    return maze[x][y];
+}
+
+void BFS(pair <int, int> src){
+    queue < pair<int, int> > temp;
+
+    temp.push(src);
+    visited[src.first][src.second] = 1;
+    //printf("\nSRC : %d %d", src.first, src.second);
+
+    while (!temp.empty()){
+        pair <int, int> node = temp.front();
+        temp.pop();
+
+        for (int i=0; i<4; i++){
+            pair <int, int> adj_node = {node.first + dX[i], node.second + dY[i]};
+
+            if (inMaze(adj_node) && available(adj_node) && !visited[adj_node.first][adj_node.second]){
+                //printf("\tadj node : %d %d\n", adj_node.first, adj_node.second);
+                visited[adj_node.first][adj_node.second] = 1;
+                temp.push(adj_node);
+            }
+        }
+    }
+
+    return;
+}
+
+int main(){
+    int rooms = 0;
+    pair <int, int> node;
+
+    scanf("%d %d", &n, &m);
+    getchar();
+    for (int row=0; row<n; row++){
+        char str[m+1];
+        gets(str);
+
+        for (int col=0; col<m; col++){
+            if (str[col] == '#'){
+                maze[row][col] = 0;
+            }else{
+                maze[row][col] = 1;
+            }
+        }
+    }
+/*
+    // To print the maze
+    for (int row=0; row<n; row++){
+        for (int col=0; col<m; col++){
+            printf("%d\t", maze[row][col]);
+        }
+        printf("\n");
+    }
+*/
+    for (int row=0; row<n; row++){
+        for (int col=0; col<m; col++){
+            if (maze[row][col] && !visited[row][col]){
+                rooms++;
+                node = {row, col};
+                BFS(node);
+            }
+        }
+    }
+
+    printf("%d", rooms);
+
+    return 0;
+}
