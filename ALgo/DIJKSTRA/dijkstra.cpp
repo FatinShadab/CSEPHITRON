2747eb4a8fdf55815146135a2ab9b5fbf0516954
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2023-02-16 12:45:50 +0600

    Update dijkstra.cpp
    completed dijkstra O(N^2) implementation

--- ALgo/DIJKSTRA/dijkstra.cpp
+++ ALgo/DIJKSTRA/dijkstra.cpp
@@ -2,11 +2,44 @@
 using namespace std;
 int n, e;
 const int N = 1e5 + 5;
-const int ∞ = 1e9; // depends on the constent limit 
+const int INF = 1e9; // depends on the constent limit 
 
 vector <pair<int, int>> adj_list[N];
+int d[N], visited[N];
 
-void dijkstra(pair <int, int> src){
+void dijkstra(int src){
+    for (int i = 1; i <= n; i++){
+        d[i] = INF;
+    }
+
+    d[src] = 0;
+
+    for (int i = 1; i <= n; i++){
+        int selectedNode = -1;
+        for (int j = 1; j <= n; j++){
+            if (visited[j]){
+                continue;
+            }
+            if (selectedNode == -1){
+                selectedNode = j;
+            }
+            if (d[j] < d[selectedNode]){
+                selectedNode = j;
+            }
+        }
+
+        visited[selectedNode] = 1;
+
+        for (auto adjEntry : adj_list[selectedNode]){
+            int adjNode = adjEntry.first;
+            int edgeCst = adjEntry.second; 
+
+            if (d[selectedNode] + edgeCst < d[adjNode]){
+                d[adjNode] = d[selectedNode] + edgeCst;
+            }
+        }
+    }
+
     return;
 }
 
@@ -20,6 +53,14 @@ int main(){
         adj_list[u].push_back(make_pair(v, w));
         adj_list[v].push_back(make_pair(u, w));
     }
+
+    int src = 1;
+    dijkstra(src);
+
+    for (int i = 1; i <= n; i++){
+        cout << d[i] << " ";
+    }
+    cout<<endl;
 
     return 0;
 }
